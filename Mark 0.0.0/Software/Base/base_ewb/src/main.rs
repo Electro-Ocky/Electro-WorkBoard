beba193e98e08d84bfc55efa0c9b781af09d6a60
Electro_Ocky <OCKYELETRICALRAIN@GMAIL.COM>
2024-01-15 00:38:59 -0300

    A
    A

--- Mark 0.0.0/Software/Base/base_ewb/src/main.rs
+++ Mark 0.0.0/Software/Base/base_ewb/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
     println!("
     
+    Projeto pessoal do Ocky:
+
+    ███████╗██╗     ███████╗ ██████╗████████╗██████╗  ██████╗     ██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗██████╗  ██████╗  █████╗ ██████╗ ██████╗ 
+    ██╔════╝██║     ██╔════╝██╔════╝╚══██╔══╝██╔══██╗██╔═══██╗    ██║    ██║██╔═══██╗██╔══██╗██║ ██╔╝██╔══██╗██╔═══██╗██╔══██╗██╔══██╗██╔══██╗
+    █████╗  ██║     █████╗  ██║        ██║   ██████╔╝██║   ██║    ██║ █╗ ██║██║   ██║██████╔╝█████╔╝ ██████╔╝██║   ██║███████║██████╔╝██║  ██║
+    ██╔══╝  ██║     ██╔══╝  ██║        ██║   ██╔══██╗██║   ██║    ██║███╗██║██║   ██║██╔══██╗██╔═██╗ ██╔══██╗██║   ██║██╔══██║██╔══██╗██║  ██║
+    ███████╗███████╗███████╗╚██████╗   ██║   ██║  ██║╚██████╔╝    ╚███╔███╔╝╚██████╔╝██║  ██║██║  ██╗██████╔╝╚██████╔╝██║  ██║██║  ██║██████╔╝
+    ╚══════╝╚══════╝╚══════╝ ╚═════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝      ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ 
+                                                                                                                                              
+
     Olá! Este é o projeto Eletro WorkBoard!
     Uma simples bancada de eletrônica portátil feita para o braço
     Com ela, você poderia medir, alterar e enviar qualquer sinal.
@@ -8,7 +18,31 @@ fn main() {
     
     *****
 
-    Não criamos um sistema operacional ainda, mas um programa Rust simples que engloba tudo,
+    .d8888b.            .d888 888                                           
+    d88P  Y88b          d88P"  888                                           
+    Y88b.               888    888                                           
+     "Y888b.    .d88b.  888888 888888 888  888  888  8888b.  888d888 .d88b.  
+        "Y88b. d88""88b 888    888    888  888  888     "88b 888P"  d8P  Y8b 
+          "888 888  888 888    888    888  888  888 .d888888 888    88888888 
+    Y88b  d88P Y88..88P 888    Y88b.  Y88b 888 d88P 888  888 888    Y8b.     
+     "Y8888P"   "Y88P"  888     "Y888  "Y8888888P"  "Y888888 888     Y8888                                                   
+
+    *****
+
+    Não criamos um sistema operacional ainda, mas um programa Rust simples que engloba tudo.
+    Nosso sistema é simples, categorizado em palavras, e baseado em árvore...
+    Você tem opções simples, que levam a outras opções, que fazem ações.
+    Há 3 tipos de ação: Medição, Geração e Transformação.
+    Há 2 tipos de dado: Valor e Função. 
+    Há 12 tipos de escalar: 10^-12(Tera), até, 10^12.
+    Há um número indefinido de áreas: Área 1 , Área 2, Área 3.
+    Exemplo: Eletrônica > Valor > Voltagem > Alternada.
+
+    Ele é assim para ser simples, ser acessado com controles simples, com poucos botões.
+
+    Segundo o Hardware padrão do projeto, há o mini-controle formado de:
+        Jostick com click, navegação entre árvore de opções, e clique para confirmação de opção.
+        Coleção de 4 Botões, cima e baixo para aumentar/dimunir escala; esquerada/direita para mudar de ação. 
 
     -
 
@@ -16,6 +50,9 @@ fn main() {
     O sistema operacional, que tem o sistema operacional para o qual o código foi feito e seus softwares,
     A base que é sobre o código base usado para interligar todos os sub-códigos,
     Os arquivos, com drivers, recursos avançados externos...
+
+    -
+
 
     *****
 
